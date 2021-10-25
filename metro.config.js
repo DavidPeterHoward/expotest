967b7271c4ad64d89a69bd04aa1ee3a39c23c09e
David <david.h@labrys.io>
2021-10-25 10:40:57 +1000

    Configure EAS Build

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-module.exports = getDefaultConfig(__dirname);
+module.exports = {
+    resolver: {
+        sourceExts: ["js", "jsx", "ts", "tsx"],
+    },
+    transformer: {
+        assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+    },
+};
