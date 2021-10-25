42f2f43dc8d3faaf4c90f5ccd19c17a43425be48
David <david.h@labrys.io>
2021-10-25 10:19:07 +1000

    Configure EAS Build

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Realm from "realm";
+
+const app = new Realm.App({ id: "application-0-onlxj" });
 
 export default function App() {
+  console.log(app);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -12,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
